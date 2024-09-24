5cbf7e53025ebe9e828bdf0a3641de2c38901e1a
jugunu-21 <aaruhijugunu@gmail.com>
2024-09-24 11:17:36 +0530

    correct grammar mistakes

--- components/atomics/AppProject.jsx
+++ components/atomics/AppProject.jsx
@@ -43,7 +43,7 @@ const AppProject = ({ project,id }) => (
        
         <hr className='border-b-2 w-16 mt-4'/>
         <div className=" flex justify-between space-x-4">
-        <Link  href={project.url} className=' flex transition duration-300 transform translate-y-2 hover:translate-y-0 hover:text-primary' > <div className='text-sm font-semibold px-1'>Live Project</div><Image  height={18} width={18} src="External Link Icon.svg"></Image></Link>
+        <Link  href={project.url} target="blank" className=' flex transition duration-300 transform translate-y-2 hover:translate-y-0 hover:text-primary' > <div className='text-sm font-semibold px-1'>Live Project</div><Image  height={18} width={18} src="External Link Icon.svg"></Image></Link>
         <Link className="transition duration-300 transform translate-y-2 hover:translate-y-0 " href={project.giturl} > <Image   height={20} width={20} src="skills/skill-github.svg"></Image></Link>
         </div></div>
       {project.name === "Urstyle" &&
