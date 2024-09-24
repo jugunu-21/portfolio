fb2f8f6e5ce63222c1007280c1f046863035f07c
jugunu-21 <aaruhijugunu@gmail.com>
2024-09-24 11:29:31 +0530

    rearrange the index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,28 +8,7 @@ import AppShell from '../components/templates/AppShell';
 const Home = () => (
   <div className="scroll-smooth">
     <AppShell hero>
-      <AppSection title="What can I do">
-        <div className="grid grid-cols-1 gap-3 md:grid-cols-2 xl:grid-cols-3 lg:gap-4">
-          {USER.services.map((item) => (
-            <div
-              key={item.id}
-              className="p-6 border rounded-lg border-light-gray"
-            >
-              <div className="flex items-center">
-                <Image src={item.icon} alt={item.name} width={40} height={40} />
-                <h3 className="ml-4 text-sm font-semibold text-black lg:text-base">
-                  {item.name}
-                </h3>
-              </div>
-              <div className="mt-3">
-                <p className="text-xs lg:text-sm text-gray">
-                  {item.description}
-                </p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </AppSection>
+     
       <AppSection title="Skill's & Technology">
         <div className="grid grid-cols-2 gap-3 lg:grid-cols-3 xl:grid-cols-4">
           {USER.skills.map((item) => (
@@ -67,6 +46,28 @@ const Home = () => (
             Show all my projects <i className="fas fa-chevron-right" />
           </Link>
         </div> */}
+      </AppSection>
+      <AppSection title="What can I do">
+        <div className="grid grid-cols-1 gap-3 md:grid-cols-2 xl:grid-cols-3 lg:gap-4">
+          {USER.services.map((item) => (
+            <div
+              key={item.id}
+              className="p-6 border rounded-lg border-light-gray"
+            >
+              <div className="flex items-center">
+                <Image src={item.icon} alt={item.name} width={40} height={40} />
+                <h3 className="ml-4 text-sm font-semibold text-black lg:text-base">
+                  {item.name}
+                </h3>
+              </div>
+              <div className="mt-3">
+                <p className="text-xs lg:text-sm text-gray">
+                  {item.description}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
       </AppSection>
     </AppShell>
   </div>
